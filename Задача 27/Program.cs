136656ee5842ed811aee1b3e63afd7cba287526d
And_Sukhanov <Andrey.sx@mail.ru>
2022-10-26 20:00:34 +0300

    Домашнее задание к семинару 4

---
+++ Задача 27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+int Sum =0;
+A = Math.Abs(A);
+while (A > 9)
+    {
+    Sum = Sum + A %10;
+    A = A/10;
+    }
+Console.WriteLine(Sum+A%10);
---
+++ Задача 29/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] array = { 1, 33, 3, 5, 6, 34, 89, 22 };
+        Console.Write("[");
+        Console.Write(String.Join(",", array));
+        Console.Write("]");
